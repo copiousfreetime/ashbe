df546d18e07ca62904a706fd35fc5dd329049848
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-03-22 19:00:24 -0600

    refactoring out inheritence

--- lib/ashbe/column_family.rb
+++ lib/ashbe/column_family.rb
@@ -1,9 +1,12 @@
+require 'forwardable'
+
 module Ashbe
   # 
   # A container holding all the qualifiers in a column family for a particular
   # Row
   #
   class ColumnFamily 
+    include Enumerable
 
     # The name of this column family
     attr_reader :name
@@ -11,10 +14,36 @@ module Ashbe
     # all the qualifiers that are held in this column family
     attr_reader :qualifiers
 
+    ### Hash Delegation ###
+    extend Forwardable
+    def_delegators :@qualifiers,
+      :empty?, :length, :size,
+      :[], :[]=, :has_key?, :fetch, :store, :keys, :values,
+      :each, :each_pair, :each_key, :each_value
+
     def initialize( name )
       @name       = name
       @qualifiers = {}
     end
+    alias_method :has_qualifier?, :has_key?
 
+    #
+    # Add a qualifier to the column family
+    #
+    def add_qualifier( qual )
+      @qualifiers[qual.name] = qual
+    end
+    alias << add_qualifier
+
+    #
+    # shortcut method to adding a qualifier and its value
+    # to the column family directly without having to create
+    # a Qualifier instance
+    #
+    def store(qualifier_name, value )
+      qual = Qualifier.new( qualifier_name, value )
+      add_qualifier( qual )
+    end
+    alias []= store
   end
 end
--- test/ashbe/column_family_spec.rb
+++ test/ashbe/column_family_spec.rb
@@ -8,4 +8,15 @@ describe Ashbe::ColumnFamily do
   it "should have a name" do
     @cf.name.must_equal "foo"
   end
+
+  it "can add a new qualifier" do
+    q = ::Ashbe::Qualifier.new( 'wibble', "snorf" )
+    @cf << q
+    @cf['wibble'].last.value.must_equal "snorf"
+  end
+
+  it "can add a new qualifier via a shortcut" do
+    @cf['wibble'] = "snorf"
+    @cf['wibble'].last.value.must_equal "snorf"
+  end
 end
