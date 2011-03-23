28eb20fbd669599c1d07518f05c9b62c3bc3d31b
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-03-22 20:59:21 -0600

    for once, actually put data into the table

--- lib/ashbe/table.rb
+++ lib/ashbe/table.rb
@@ -2,18 +2,23 @@ require 'ashbe'
 
 module Ashbe
   class Table
+
     java_implements "org.apache.hadoop.hbase.client.HTableInterface"
 
-    # Name of the table
-    attr_reader :table_name
+    # The meta data about the table
+    attr_reader :meta
 
     # The ::Ashbe::Configuration used to connect
     attr_reader :config
 
     def initialize( table_name, config = Configuration.new )
-      @table_name = table_name
       @config     = config
-      @htable     = ::Ashbe::Java::HTable.new( @config, @table_name )
+      @htable     = ::Ashbe::Java::HTable.new( @config, table_name )
+      @meta       = Table::Meta.from_htable( @htable.getTableDescriptor )
+    end
+
+    def name
+      @meta.name
     end
 
     def is_auto_flush?
@@ -21,9 +26,27 @@ module Ashbe
     end
 
     #
-    # Put the given data into 
-    def put( row_or_key, data = {} )
+    # TODO: figure out how to extrac all the methods from HTableInterface and
+    # delegate them to @htable
+    #
 
+    #
+    # Put the given row key and row data into the table.  The row_data
+    # is a 2 level hash, top level being column family names, the second level
+    # are qualifiers and the values there are strings, i.e.:
+    #
+    #   {
+    #     'foo_column' => { 'qual1' => 'val1', ... },
+    #     'bar_column' => { 'quala' => 'vala', ... },
+    #     ....
+    #   }
+    #
+    # This will return the Asbhe::Row object of the data that was inserted
+    #
+    def put( row_key, row_data = {} )
+      row = ::Ashbe::Row.new( row_key, row_data )
+      @htable.put( row.to_put )
+      return row
     end
   end
 end
--- test/ashbe/table_spec.rb
+++ test/ashbe/table_spec.rb
@@ -8,7 +8,7 @@ describe Ashbe::Table do
     @table_name = "test_table"
     @families   = %w[ foo bar baz ].collect { |c| Ashbe::ColumnFamily::Meta.new( c ) }.sort_by{ |f| f.name }
     @admin.create_table( @table_name, @families )
-    @conn       = ::Ashbe::Table.new( @table_name, @config )
+    @table      = ::Ashbe::Table.new( @table_name, @config )
   end
 
   after do
@@ -18,16 +18,20 @@ describe Ashbe::Table do
   end
 
   it "can connect to a table" do
-    tc = ::Ashbe::Table.new( @table_name, @config )
-    tc.is_auto_flush?.must_equal true
+    t = ::Ashbe::Table.new( @table_name, @config )
+    t.is_auto_flush?.must_equal true
+  end
+
+  it "can access the table's meta data" do
+    @table.name.must_equal @table_name
+    @table.meta.column_families.size.must_equal @families.size
   end
 
   it "can put a row into the table" do
-    row = @conn.put( 12345, { 'foo' => { '1' => 'one',  '2' => 'two' },
-                              'bar' => { '4' => 'four', '6' => 'six' },
-                              'baz' => { '42' => 'a' } } )
-    # TODO:
-    #row.key.must_equal 12345
-    #row.column_families.size.must_equal 3
+    row = @table.put( 12345, { 'foo' => { '1' => 'one',  '2' => 'two' },
+                               'bar' => { '4' => 'four', '6' => 'six' },
+                               'baz' => { '42' => 'a' } } )
+    row.key.must_equal 12345
+    row.column_families.size.must_equal 3
   end
 end
