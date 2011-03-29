16bd85e58e8ab987573a5e8f871a8b04d326d928
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-03-29 15:51:00 -0600

    refactor Bytes module into a BytesConversions and to modules ToBytes FromBytes

--- lib/ashbe/bytes.rb
+++ lib/ashbe/bytes.rb
@@ -1,28 +1,76 @@
 require 'ashbe'
 
 module Ashbe
-  module Bytes
-    def to_bytes( something )
-      ::Ashbe::Java::Bytes.toBytes( something )
+  class BytesConversion
+    #
+    # to_bytes( anything ) -> Java::byte[]
+    #
+    def self.to_bytes( anything )
+      ::Ashbe::Java::Bytes.toBytes( anything )
     end
 
-    def to_string( bytes )
+    #
+    # is_bytes?( anything ) -> false | true
+    #
+    def self.is_bytes?( anything )
+      anything.instance_of?( ::Java::byte[] )
+    end
+
+    #
+    # to_string( Java::bytes[] ) -> String
+    #
+    def self.to_string( bytes )
       ::Ashbe::Java::Bytes.toString( bytes )
     end
 
     #
+    # to_fixnum( Java::bytes[] ) -> Fixnum
+    #
     # Ruby fixnums, are backed by java Longs
     #
-    def to_fixnum( bytes )
+    def self.to_fixnum( bytes )
       ::Ashbe::Java::Bytes.toLong( bytes )
     end
 
+    #
+    # to_float( Java::bytes[] ) -> Float
+    #
     # We mean a ruby Float here, not a 4 byte IEEE float
     # In jruby, ruby Float objects are backed by java Doubles
-    def to_float( bytes )
+    #
+    def self.to_float( bytes )
       ::Ashbe::Java::Bytes.toDouble( bytes )
     end
+  end
 
-    extend self
+  #
+  # Include or extend on classes to convert thme to Java::byte[]
+  #
+  module ToBytes
+    def to_bytes
+      Ashbe::BytesConversion.to_bytes( self )
+    end
+
+    def is_bytes?
+      Ashbe::BytesConversion.is_bytes?( self )
+    end
+  end
+
+  #
+  # Include or extend this on classes that represent Java::byte[] so they my be
+  # converted easily to Strings, Fixnums or Floats
+  #
+  module FromBytes
+    def to_string
+      ::Ashbe::BytesConversion.to_string( self )
+    end
+
+    def to_fixnum
+      ::Ashbe::BytesConversion.to_fixnum( self )
+    end
+
+    def to_float
+      ::Ashbe::BytesConversion.to_float( self )
+    end
   end
 end
--- test/ashbe/bytes_spec.rb
+++ test/ashbe/bytes_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
-describe Ashbe::Bytes do
+describe Ashbe::BytesConversion do
 
   it "can round trip an Fixnum" do
     bytes = 12345.to_bytes
-    i = Ashbe::Bytes.to_fixnum( bytes )
-    puts i.class
+    bytes.extend( Ashbe::FromBytes )
+    i = bytes.to_fixnum
     i.must_equal 12345
   end
   
   it "can round trip a Float" do
     bytes = (42.42).to_bytes
-    f = Ashbe::Bytes.to_float( bytes )
+    bytes.extend( Ashbe::FromBytes )
+    f = bytes.to_float
     f.must_equal( 42.42 )
 
   end
 
   it "can round trip a string" do
     bytes = "the answer".to_bytes
-    s = Ashbe::Bytes.to_string( bytes )
+    bytes.extend( Ashbe::FromBytes )
+    s = bytes.to_string
     s.must_equal "the answer"
   end
 
